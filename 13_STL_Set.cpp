ce06ee9299e4886632193aa3cab8797310f5cae4
Devender Verma <85267939+Crishpy-Coffe@users.noreply.github.com>
2022-08-29 15:44:12 +0530

    Lecture 13

--- 13_STL_Set.cpp
+++ 13_STL_Set.cpp
@@ -1,35 +1,33 @@
-#include<bits/stdc++.h>
-#include<set>
+#include <bits/stdc++.h>
+#include <set>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-// set me same elements ko 1 hi element manta hai like 3 baar 5 input diya to set me 1 baar hi aayega 
+    // set me same elements ko 1 hi element manta hai like 3 baar 5 input diya to set me 1 baar hi aayega
 
-set<int> s;
+    set<int> s;
 
+    s.insert(3);
+    s.insert(2);
+    s.insert(4);
+    s.insert(5);
+    s.insert(6);
+    s.insert(7);
+    // begin()
+    // end()
+    // size()
+    // max_size()
+    // empty()
+    // erase(3)
 
-s.insert(3);
-s.insert(2);
-s.insert(4);
-s.insert(5);
-s.insert(6);
-s.insert(7);
-// begin()
-// end()
-// size()
-// max_size()
-// empty()
-// erase(3)
+    // for print
+    for (auto i : s)
+    {
+        cout << i << endl;
+    }
 
-// for print
-for(auto i:s){
-    cout << i << endl; 
-}
-
-
-
-
-return 0;
+    return 0;
 }
