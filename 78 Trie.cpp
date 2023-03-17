f61d3363ef77b83eefbe9f474ab08384861d979a
Devender Verma <85267939+Crishpy-Coffe@users.noreply.github.com>
2023-03-17 11:33:49 +0530

    Updation

--- 78 Trie.cpp
+++ 78 Trie.cpp
@@ -109,5 +109,69 @@ int main(){
     cout << "node is present : " << node->search("de") << endl;
     node->deleted("deve");
 
-return 0;
-}afsdasdfasfd
+    return 0;
+}
+
+//making trie by iteration
+class Trie {
+private:
+    class Node{
+     public:
+        Node* node[26]={};
+        bool isword= false;
+    };
+    
+public:
+    Node* ROOT;
+    Trie() {
+        ROOT= new Node();
+    }
+    //Fixed memory Leak but it will take twice the time.
+    ~Trie(){
+        // clear(ROOT);
+    }
+    
+    void insert(string word) {
+        Node* root = ROOT;
+        for(char x: word){
+            x-='a';
+            if(root->node[x]==NULL){
+                root->node[x]= new Node();
+            }
+            root = root->node[x];
+        }
+        root->isword= true;
+    }
+    
+    bool search(string word) {
+        Node* root = ROOT;
+        for(auto i: word){
+            char x = i-'a';
+            if(root->node[x]!=NULL){
+                root = root->node[x];
+            }else return false;
+        }
+        return root->isword;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* root = ROOT;
+        for(auto i: prefix){
+            char x = i-'a';
+            if(root->node[x]!=NULL){
+                root = root->node[x];
+            }else return false;
+        }
+        return root!=NULL;
+    }
+    
+    void clear(Node* root){
+        for(int i=0; i<26; i++){
+            if(root->node[i]!= nullptr){
+                clear(root->node[i]);
+            }
+        }
+        cout<<root<<" ";
+        delete root;
+    }
+};
