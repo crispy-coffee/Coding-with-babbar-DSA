748fdbd4cfacce0c64f3fd59c180b2d122daf854
Devender Verma <85267939+Crishpy-Coffe@users.noreply.github.com>
2023-04-16 17:25:09 +0530

    Lecture 131: Buy and Sell Stock 2

---
+++ L-131 Buy and Sell Stock 2.cpp
@@ -0,0 +1,120 @@
+class Solution {
+public:
+    // 1 Recursion
+    int Recursion(vector<int>& prices, int index, bool buy){
+        if(index == prices.size())
+            return 0;
+
+        //Buy = True means we can buy
+        int profit = 0;
+        if(buy){
+            int buykaro = -prices[index] +  Recursion(prices, index+1, false);
+            int sellkaro =  Recursion(prices, index+1, true);
+            profit = max(buykaro, sellkaro);
+        }else{
+            int buykaro = prices[index] +  Recursion(prices, index+1, true);
+            int sellkaro =  Recursion(prices, index+1, false);
+            profit = max(buykaro, sellkaro);
+        }
+
+        return profit;
+    }
+
+    // 2 Recursion With Memoization
+    int Memoization(vector<int>& prices, int index, bool buy, vector<vector<int>>& dp){
+        if(index == prices.size())
+            return 0;
+
+        if(dp[index][buy] != -1){
+            return dp[index][buy];
+        }
+
+        //Buy = True means we can buy
+        int profit = 0;
+        if(buy){
+            int buykaro = -prices[index] +  Memoization(prices, index+1, false, dp);
+            int sellkaro =  Memoization(prices, index+1, true, dp);
+            profit = max(buykaro, sellkaro);
+        }else{
+            int buykaro = prices[index] +  Memoization(prices, index+1, true, dp);
+            int sellkaro =  Memoization(prices, index+1, false, dp);
+            profit = max(buykaro, sellkaro);
+        }
+
+        return dp[index][buy] = profit;
+    }
+
+    // 3 Tabulation
+    int Tabulation(vector<int>& prices){
+        int n = prices.size();
+        vector<vector<int>> dp(n+2, vector<int>(2, 0));
+
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    int buykaro = -prices[index] +  dp[index+1][false];
+                    int sellkaro = dp[index+1][true];
+                    profit = max( buykaro, sellkaro);
+                }else{
+                    int buykaro = prices[index] + dp[index+1][true];
+                    int sellkaro = dp[index+1][false];
+                    profit = max( buykaro, sellkaro);
+                }
+                dp[index][buy] = profit;
+            }   
+        }
+        return dp[0][true];
+    }
+
+    // 4 Space Optimization
+    int SpaceOptimization(vector<int>& prices){
+        int n = prices.size();
+        // vector<vector<int>> dp(n+2, vector<int>(2, 0));
+        vector<int> curr(2, 0), next(2, 0);
+
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    int buykaro = -prices[index] +  next[false];
+                    int sellkaro = next[true];
+                    profit = max( buykaro, sellkaro);
+                }else{
+                    int buykaro = prices[index] + next[true];
+                    int sellkaro = next[false];
+                    profit = max( buykaro, sellkaro);
+                }
+                curr[buy] = profit;
+            }
+            next = curr;   
+        }
+        return curr[true];
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        // 1 Recursion
+        // return Recursion(prices, 0, true);
+
+        // 2 Recursion With Memoization
+        // vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        // return Memoization(prices, 0, true, dp);
+
+        // 3 Tabulation
+        // return Tabulation(prices);
+
+        // 4 Space Optimization
+        return SpaceOptimization(prices);
+
+        // Khatarnak approach 
+        /*
+        int profit = 0;
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i-1]<prices[i])
+                profit+= prices[i]-prices[i-1];
+        }
+        return profit;
+        */
+    }
+};
